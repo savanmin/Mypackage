de1421cd223aa9bebcc6f64f5b54e391509608db
Savan Minsariya <savanminsariya18@gmail.com>
2018-02-08 15:24:57 +0530

    [ADDED] Adding new files into Mypackage for new module

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# -*- © Savan Minsariya 2018 -*-
+{
+    'name' : 'Student management system',
+    'version' : 'Beta',
+    'summary': ' ',
+    'sequence': 0,
+    'description': """Nothing yet""",
+    'category': 'New Package',
+    'website': " ",
+    'images' : [" "],
+    'depends' : [" "],
+    'data': ["Nothing yet"],
+    'qweb': [" "],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+    'post_init_hook': ' ',
+}
